9150166fa9c5c5f5b5bf6272b168ae15bd2c8313
Tatiana Barga <tatibspersonal@gmail.com>
2024-06-17 21:27:39 -0500

    set everything up

---
+++ rarev2api/models/users.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Users(models.Model):
+  first_name = models.CharField(max_length=50) 
+  last_name = models.CharField(max_length=50)
+  bio = models.CharField(max_length=50)
+  profile_image_url = models.CharField(max_length=50)
+  email = models.CharField(max_length=50)
+  created_on = models.DateField()
+  active = models.BooleanField(default=False)
+  is_staff = models.BooleanField(default=False)
+  uid = models.CharField(max_length=50)
